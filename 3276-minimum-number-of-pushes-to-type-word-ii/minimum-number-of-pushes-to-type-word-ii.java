a30632b012946714b9b66072a4f3f58b99be90b1
Saurya Kapoor <sauryakapoor1506@gmail.com>
2026-07-31 22:46:38 +0530

    Time: 8 ms (99.71%) | Memory: 48.3 MB (17.34%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -2,21 +2,16 @@ import java.util.Arrays;
 
 class Solution {
     public int minimumPushes(String word) {
-        int[] frequency = new int[26];
-        for (int i = 0; i < word.length(); i++) {
-            frequency[word.charAt(i) - 'a']++;
+        int[] freq = new int[26];
+        for (int i = 0, n = word.length(); i < n; i++) {
+            freq[word.charAt(i) - 'a']++;
         }
-        Arrays.sort(frequency);
-        int answer = 0;
-        int position = 0;
-        for (int i = 25; i >= 0; i--) {
-            if (frequency[i] == 0) {
-                break;
-            }
-            int pushes = (position / 8) + 1;
-            answer += frequency[i] * pushes;
-            position++;
+        Arrays.sort(freq);
+        int result = 0;
+        int rank = 0;
+        for (int i = 25; i >= 0 && freq[i] != 0; i--, rank++) {
+            result += freq[i] * (rank / 8 + 1);
         }
-        return answer;
+        return result;
     }
 }
